fecbe17852eae1235b3086630ede070a39062947
f3anor <lucas.kahlert@square-src.de>
2014-12-08 02:09:05 +0100

    Create benchmark script

---
+++ exercise_2_fast_median_filter/benchmark.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+range=$(seq 1 30)
+
+for image in "fruits.jpg" "green-eye1.jpg"; do
+    for radius in $range; do
+        perf stat \
+            --repeat 5 \
+            --output bench-$image.log \
+            --append \
+            --detailed \
+            ./fast_median_filter --radius $radius --target out.jpg $image
+    done;
+done;

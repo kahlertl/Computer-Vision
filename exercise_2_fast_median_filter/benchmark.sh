410131f50d5c14c6e3d866e87ea8f15ac230caa6
f3anor <lucas.kahlert@square-src.de>
2014-12-08 16:39:26 +0100

    Uncomment radius benchmark

--- exercise_2_fast_median_filter/benchmark.sh
+++ exercise_2_fast_median_filter/benchmark.sh
@@ -2,16 +2,16 @@
 
 [ ! -d "stats" ] && mkdir stats
 
-# for image in "fruits.jpg" "green-eye.jpg"; do
-#     for radius in $(seq 1 30); do
-#         perf stat \
-#             --repeat 5 \
-#             --output bench-$image.log \
-#             --append \
-#             --detailed \
-#             ./fast_median_filter --radius $radius --target out.jpg $image
-#     done
-# done
+for image in "fruits.jpg" "green-eye.jpg"; do
+    for radius in $(seq 1 30); do
+        perf stat \
+            --repeat 5 \
+            --output bench-$image.log \
+            --append \
+            --detailed \
+            ./fast_median_filter --radius $radius --target out.jpg $image
+    done
+done
 
 echo "# Show linar time complexity in the image size" > stats/resize.log
 for image in imgs/*.jpg; do

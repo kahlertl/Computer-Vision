ee50fb52d6ab7cabd50306c4d300df05b1647c55
f3anor <lucas.kahlert@square-src.de>
2014-12-11 23:47:44 +0100

    Make histogram equalization more modular

--- lab_3_histogram_gray/histo_gray.cpp
+++ lab_3_histogram_gray/histo_gray.cpp
@@ -6,12 +6,49 @@
 using namespace std;
 using namespace cv;
 
+void cumsum(const Mat &hist, Mat &cdf_matrix)
+{
+    // reset CDF matrix
+    cdf_matrix = Mat::zeros(hist.size(), hist.type());
+
+    float sum = 0;
+
+    for (int row = 0; row < hist.rows; row++) {
+        sum += hist.at<float>(row, 0);
+        cdf_matrix.at<float>(row, 0) = sum;
+    }
+}
+
+
+/**
+ * Draw the histograms for B, G and R
+ */
+void draw_histogram(const Mat &hist, Mat &canvas, Scalar color = {255, 255, 255})
+{
+    const int hist_width  = canvas.cols;
+    const int hist_height = canvas.rows; 
+    const int bin_width   = cvRound((double) hist_width / hist.rows);
+    
+    // normalize the result to [ 0, canvas.rows ]
+    Mat normalized;
+    normalize(hist, normalized, 0, canvas.rows - 10, NORM_MINMAX, -1, Mat());
+
+    for (int i = 1; i < normalized.rows; i++) {
+        line(
+            canvas,
+            Point(bin_width * (i - 1), hist_height - cvRound(normalized.at<float>(i - 1))),
+            Point(bin_width * (i),     hist_height - cvRound(normalized.at<float>(i)) ),
+            color,
+            2, 8, 0
+        );
+    }
+}
 
 
 int main(int argc, char** argv)
 {
-    Mat src;
-    Mat dst;
+    Mat image;
+    Mat equalized;
 
     if (argc != 2) {
         cout << "Usage: ./histo_match image" << endl;
@@ -20,17 +57,13 @@ int main(int argc, char** argv)
     }
 
     // Load image
-    src = imread( argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+    image = imread( argv[1], CV_LOAD_IMAGE_GRAYSCALE);
 
-    if( !src.data ) {
+    if( !image.data ) {
         cerr << "Error cannot read " << argv[1] << endl;
 
         return 1;
     }
-
-    // Separate the image in 3 places (B, G and R)
-    // vector<Mat> bgr_planes;
-    // split(src, bgr_planes);
 
     // Establish the number of bins
     int hist_size = 256;
@@ -45,60 +78,33 @@ int main(int argc, char** argv)
     Mat hist;
 
     // compute the histogram
-    calcHist(&src, 1, 0, Mat(), hist, 1, &hist_size, &hist_range, uniform, accumulate);
+    calcHist(&image, 1, 0, Mat(), hist, 1, &hist_size, &hist_range, uniform, accumulate);
 
     // compute cumulative distribution function
-    Mat cdf = Mat::zeros(hist.size(), hist.type());
-
-    float sum = 0;
-    for (int row = 0; row < hist.rows; row++) {
-        sum += hist.at<float>(row, 0);
-        cdf.at<float>(row, 0) = sum;
-    }
-
-    // cout << hist << endl;
-    // cout << cdf << endl;
-
-    // draw the histograms for B, G and R
+    Mat cdf;
+    cout << "Reached" << endl;
+    cumsum(hist, cdf);
+        
     int hist_width  = 512;
     int hist_height = 400;
-    int bin_width   = cvRound((double) hist_width / hist_size);
 
     Mat hist_image(hist_height, hist_width, CV_8UC3, Scalar( 0,0,0));
 
-    // normalize the result to [ 0, hist_image.rows ]
-    normalize(hist, hist, 0, hist_image.rows - 10, NORM_MINMAX, -1, Mat());
-    normalize(cdf,  cdf,  0, hist_image.rows - 10, NORM_MINMAX, -1, Mat());
-
-    for(int i = 1; i < hist_size; i++) {
-        line(
-            hist_image,
-            Point(bin_width * (i - 1), hist_height - cvRound(hist.at<float>(i - 1))),
-            Point(bin_width * (i),     hist_height - cvRound(hist.at<float>(i)) ),
-            Scalar(225, 225, 225),
-            2, 8, 0
-        );
-        line(
-            hist_image,
-            Point(bin_width * (i - 1), hist_height - cvRound(cdf.at<float>(i - 1))),
-            Point(bin_width * (i),     hist_height - cvRound(cdf.at<float>(i)) ),
-            Scalar(0, 0, 225),
-            2, 8, 0
-        );
-    }
+    draw_histogram(hist, hist_image, {  0,   0, 255});
+    draw_histogram(cdf , hist_image, {255,   0,   0});
 
     // apply histogram equalization
-    equalizeHist(src, dst);
+    equalizeHist(image, equalized);
 
     // display
     namedWindow("Source", CV_WINDOW_AUTOSIZE);
-    imshow("Source", src);
+    imshow("Source", image);
 
     namedWindow("Histogram", CV_WINDOW_AUTOSIZE);
     imshow("Histogram", hist_image);
 
     namedWindow("Histogram Equalization", CV_WINDOW_AUTOSIZE);
-    imshow("Histogram Equalization", dst);
+    imshow("Histogram Equalization", equalized);
 
     waitKey(0);
 

b01628c3c34f2402179e1202c35a541c1e45e58a
f3anor <lucas.kahlert@square-src.de>
2014-12-12 01:01:27 +0100

    Implement histogram matching for grayscale images

--- lab_3_histogram_gray/histo_gray.cpp
+++ lab_3_histogram_gray/histo_gray.cpp
@@ -6,10 +6,13 @@
 using namespace std;
 using namespace cv;
 
+
 void cumsum(const Mat &hist, Mat &cdf_matrix)
 {
-    // reset CDF matrix
-    cdf_matrix = Mat::zeros(hist.size(), hist.type());
+    // initialize CDF matrix
+    if (cdf_matrix.empty()) {
+        cdf_matrix = Mat::zeros(hist.size(), hist.type());
+    }
 
     float sum = 0;
 
@@ -45,22 +48,85 @@ void draw_histogram(const Mat &hist, Mat &canvas, Scalar color = {255, 255, 255}
 }
 
 
+void histogram_matching(Mat &image, Mat &pattern, Mat &match)
+{
+    // initialize matched image matrix if empty
+    if (match.empty()) {
+        match = Mat::zeros(image.size(), image.type());
+    }
+    
+    // number of bins
+    int hist_size = 256;
+
+    // ranges for B,G,R
+    const float  range[]    = { 0, 256 } ;
+    const float* hist_range = { range };
+
+    // computer CDF if the image that should be matched and  of the pattern
+    // image with the nice histogram
+    Mat image_hist;
+    Mat image_cdf;
+    Mat pattern_hist;
+    Mat pattern_cdf;
+
+    calcHist(&image,   1, 0, Mat(), image_hist,   1, &hist_size, &hist_range, true, false);
+    calcHist(&pattern, 1, 0, Mat(), pattern_hist, 1, &hist_size, &hist_range, true, false);
+
+    cumsum(image_hist,   image_cdf);
+    cumsum(pattern_hist, pattern_cdf);
+
+    // create look up table
+    Mat lut = Mat::zeros(pattern_hist.size(), pattern_hist.type());
+
+    for (int i = 0; i < hist_size; i++) {
+        float frequency = image_cdf.at<float>(i);
+        int color = 0;
+
+        // search for the first CDF sum greater than the frequency of the color
+        while (color < hist_size) {
+            if (pattern_cdf.at<float>(color) >= frequency) {
+                break;
+            }
+            color++;
+        }
+
+        lut.at<int>(i) = (color < hist_size) ? color : hist_size - 1;
+    }
+
+    // Mat match = Mat::zeros(image.size(), image.type());
+
+    for (int row = 0; row < image.rows; row++) {
+        for (int col = 0; col < image.cols; col++) {
+            image.at<uchar>(row,col) = lut.at<int>(image.at<uchar>(row,col));
+        }
+    }
+}
+
+
 int main(int argc, char** argv)
 {
     Mat image;
     Mat equalized;
+    Mat pattern;
 
-    if (argc != 2) {
-        cout << "Usage: ./histo_match image" << endl;
+    if (argc != 3) {
+        cout << "Usage: ./histo_match image pattern" << endl;
 
         return 1;
     }
 
     // Load image
-    image = imread( argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+    image   = imread( argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+    pattern = imread( argv[2], CV_LOAD_IMAGE_GRAYSCALE);
 
-    if( !image.data ) {
+    if (!image.data ) {
         cerr << "Error cannot read " << argv[1] << endl;
+
+        return 1;
+    }
+
+    if (!pattern.data ) {
+        cerr << "Error cannot read " << argv[2] << endl;
 
         return 1;
     }
@@ -75,23 +141,30 @@ int main(int argc, char** argv)
     const bool uniform    = true;
     const bool accumulate = false;
 
-    Mat hist;
+    Mat hist_image;
+    Mat hist_pattern;
 
-    // compute the histogram
-    calcHist(&image, 1, 0, Mat(), hist, 1, &hist_size, &hist_range, uniform, accumulate);
+    // compute the histograms
+    calcHist(&image,   1, 0, Mat(), hist_image,   1, &hist_size, &hist_range, uniform, accumulate);
+    calcHist(&pattern, 1, 0, Mat(), hist_pattern, 1, &hist_size, &hist_range, uniform, accumulate);
 
     // compute cumulative distribution function
-    Mat cdf;
-    cout << "Reached" << endl;
-    cumsum(hist, cdf);
-        
+    Mat cdf_image;
+    Mat cdf_pattern;
+
+    cumsum(hist_image,   cdf_image);
+    cumsum(hist_pattern, cdf_pattern);
+    
+
+    // canvas for histograms and CDFs
     int hist_width  = 512;
     int hist_height = 400;
 
-    Mat hist_image(hist_height, hist_width, CV_8UC3, Scalar( 0,0,0));
+    Mat canvas_hist(hist_height, hist_width, CV_8UC3, Scalar( 0,0,0));
 
-    draw_histogram(hist, hist_image, {  0,   0, 255});
-    draw_histogram(cdf , hist_image, {255,   0,   0});
+    draw_histogram(hist_image,  canvas_hist, {  0,   0, 255});
+    draw_histogram(cdf_image ,  canvas_hist, {255,   0,   0});
+    draw_histogram(cdf_pattern, canvas_hist, {  0, 255,   0});
 
     // apply histogram equalization
     equalizeHist(image, equalized);
@@ -100,11 +173,16 @@ int main(int argc, char** argv)
     namedWindow("Source", CV_WINDOW_AUTOSIZE);
     imshow("Source", image);
 
-    namedWindow("Histogram", CV_WINDOW_AUTOSIZE);
-    imshow("Histogram", hist_image);
+    namedWindow("Histogram and CDFs", CV_WINDOW_AUTOSIZE);
+    imshow("Histogram and CDFs", canvas_hist);
 
     namedWindow("Histogram Equalization", CV_WINDOW_AUTOSIZE);
     imshow("Histogram Equalization", equalized);
+
+    histogram_matching(image, pattern, image);
+
+    namedWindow("Histogram Matching", CV_WINDOW_AUTOSIZE);
+    imshow("Histogram Matching", image);
 
     waitKey(0);
 

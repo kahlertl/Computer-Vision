d6c6979b4dae09689a156436c293de044d946ce9
f3anor <lucas.kahlert@square-src.de>
2015-01-06 17:46:03 +0100

    Does not calculate an extra patch matrix but use the image with offset instead

--- lab_4_stereo_matching/stereo_match.cpp
+++ lab_4_stereo_matching/stereo_match.cpp
@@ -6,20 +6,12 @@ using namespace std;
 using namespace cv;
 
 
-static float matchSSD(const Mat& patch, const Mat& image, const Point2i center,
-                      const int max_disparity, float* best_match, float* disparity)
+static float matchSSD(const int radius, const Mat& image, const Point2i center,
+                      const int max_disparity, float* best_match, int* disparity)
 {
-    // integer division cuts off any fractional part
-    int radius = patch.cols / 2;
-
-    // normalize the row that will be entered for SSD calculation.
-    int row = center.x - radius;
-    int col = center.y - radius;
-
     // we do not want to cast rapidly from int to float, because the SSD is an
     // integer
     int best_ssd = 0;
-    int best_shift = 0;
 
     int ssd_left = 0;
     int ssd_right = 0;
@@ -27,11 +19,15 @@ static float matchSSD(const Mat& patch, const Mat& image, const Point2i center,
     for (int offset = 0; offset < max_disparity; offset++) {
 
         // walk through the patch
-        for (int prow = 0; prow < patch.rows; prow++) {
-            for (int pcol = 0; pcol < patch.cols; pcol++) {
+        for (int prow = -radius; prow < radius; prow++) {
+            for (int pcol = -radius; pcol < radius; pcol++) {
                 // grayscale images => uchar
-                int diff_right = patch.at<uchar>(prow, pcol) - image.at<uchar>(prow + row, pcol + col + offset);
-                int diff_left  = patch.at<uchar>(prow, pcol) - image.at<uchar>(prow + row, pcol + col - offset);
+                // patch - image
+                int diff_right =   image.at<uchar>(center.y + prow, center.x + pcol)
+                                 - image.at<uchar>(center.y + prow, center.x + pcol + offset);
+
+                int diff_left  =   image.at<uchar>(center.y + prow, center.x + pcol)
+                                 - image.at<uchar>(center.y + prow, center.x + pcol - offset);
 
                 ssd_right += diff_right * diff_right;
                 ssd_left += diff_left * diff_left;
@@ -40,55 +36,51 @@ static float matchSSD(const Mat& patch, const Mat& image, const Point2i center,
 
         if (ssd_left > best_ssd) {
             best_ssd    = ssd_left;
-            best_shift  = -offset;
+            *disparity  = -offset;
         } else if (ssd_right > best_ssd) {
             best_ssd    = ssd_left;
-            best_shift  = -offset;
+            *disparity  = -offset;
         }
 
     }
 
 
     *best_match = (float) best_ssd;
-    *disparity  = (float) best_shift;
 }
 
 
 static void blockMatch(const Mat& left, const Mat& right, Mat& disparity,
                        const int radius, const int max_disparity)
 {
-    disparity = Mat::zeros(left.rows, right.rows, DataType<float>::type);
+    disparity = Mat::zeros(left.rows, right.rows, DataType<int>::type);
 
     // walk through the left image
     for (int lrow = radius; lrow < left.rows - radius; lrow++) {
 
-        // cout << "Row " << lrow << endl;
+        cout << "Row " << lrow << endl;
 
         for (int lcol = radius; lcol < left.cols - radius; lcol++) {
 
-            cout << lrow << ", " << lcol << endl;
+            // cout << lrow << ", " << lcol << endl;
 
-            Mat patch (left,
-                       Range(lrow - radius, lrow + radius),
-                       Range(lcol - radius, lcol + radius));
-
-            // imshow("Patch", patch);
-            // waitKey(3000);
-
-            float shift = 0;
-            float best_match = 0;
+            int shift = 0;
+            // float best_match = 0;
             float match = 0;
 
-            matchSSD(patch, left, Point2i(lcol, lrow), max_disparity, &match, &shift);
+            matchSSD(radius, left, Point2i(lcol, lrow), max_disparity, &match, &shift);
 
-            if (match > best_match) {
-                best_match = match;
-                disparity.at<float>(lrow, lcol) = shift;                
-            }
+            // if (match > best_match) {
+                // best_match = match;
+                disparity.at<int>(lrow, lcol) = shift;                
+            // }
 
 
         }
     }
+
+
+
+
 }
 
 
@@ -120,8 +112,8 @@ int main(int argc, char const *argv[])
         return 1;
     }
 
-    blockMatch(img_left, img_right, disparity, 50, 5);
-
+    blockMatch(img_left, img_right, disparity, 50, 25);
+    cout << disparity << endl;
 
     return 0;
 }

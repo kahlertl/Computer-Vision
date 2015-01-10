364ae4dc50a7499530a6d28f7866a933652043be
f3anor <lucas.kahlert@square-src.de>
2015-01-10 18:25:58 +0100

    Adds working 3D viewer without disparity map

---
+++ lab_4_point_cloud/viewer.cpp
@@ -0,0 +1,157 @@
+#include <iostream>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace std;
+using namespace cv;
+
+Mat image;
+Mat disparity;
+Mat canvas;
+
+int max_disp = 0;
+
+int tracker_alpha = 180;
+int tracker_beta  = 180;
+int tracker_zoom  = 100;
+int tracker_dist  = 100;
+
+/**
+ * Conversion from degree into radian
+ */
+inline float radian(const float degree)
+{
+    return degree * M_PI / 180;
+}
+
+
+/**
+ * 3D rotation matrix around x-axis
+ */
+inline Mat RotX(const float angle)
+{
+    float alpha = radian(angle);
+
+    return (Mat_<double>(3,3) << 1,          0,           0,
+                                 0, cos(alpha), -sin(alpha),
+                                 0, sin(alpha),  cos(alpha));
+}
+
+
+/**
+ * 3D rotation matrix around y-axis
+ */
+inline Mat RotY(const float angle)
+{
+    float alpha = radian(angle);
+
+    return (Mat_<double>(3,3) <<  cos(alpha), 0, sin(alpha),
+                                           0, 1,          0,
+                                 -sin(alpha), 0, cos(alpha));
+}
+
+
+/**
+ * Callback function for all trackbars
+ */
+void render(int, void*)
+{
+
+    Mat rot_x = RotX((tracker_alpha - 180) / 100.0);
+    Mat rot_y = RotY((tracker_beta  - 180) / 100.0);
+
+    double zoom = tracker_zoom / 100.0;
+    double dist = (tracker_dist - 50) / 100.0;
+
+    // reset canvas
+    canvas.setTo(0);
+
+    for (int row = 0; row < image.rows; row++) {
+        for (int col = 0; col < image.cols; col++) {
+            // Vec3b color = image.at<Vec3b>(row, col);
+
+            Mat point = (Mat_<double>(3,1) << col - image.cols / 2,
+                                              row - image.rows / 2,
+                                              1);
+
+            // rotating
+            point = rot_x * rot_y * point;
+
+            // zooming
+            point.at<double>(0,0) *= zoom;
+            point.at<double>(1,0) *= zoom;
+
+            // normalize z-component with camera distance
+            point.at<double>(0,0) /= point.at<double>(2,0) + dist;
+            point.at<double>(1,0) /= point.at<double>(2,0) + dist;
+            // point.at<double>(2,0) /= point.at<double>(2,0);
+
+            // revert centering
+            point.at<double>(0,0) += image.cols / 2;
+            point.at<double>(1,0) += image.rows / 2;
+
+            const int canvas_col = point.at<double>(0,0);
+            const int canvas_row = point.at<double>(1,0);
+
+            if (canvas_col > 0 && canvas_col < canvas.cols && canvas_row > 0 && canvas_row < canvas.rows) {
+                canvas.at<Vec3b>(canvas_row, canvas_col) = image.at<Vec3b>(row, col);
+                
+            }
+
+
+            // waitKey(0);
+        }
+    }
+
+    imshow("cloud", canvas);
+}
+
+
+
+int main(int argc, char const *argv[])
+{
+    // Argument parsing
+    if (argc != 3) {
+        cerr << "Usage: ./camera_perspective <image> <disparity map>" << endl;
+
+        return 1;
+    }
+
+    image     = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+    disparity = imread(argv[2], CV_LOAD_IMAGE_GRAYSCALE);
+
+    if (image.empty()) {
+        cerr << "Cannot read the image file." << endl;
+
+        return 1;
+    }
+
+    if (disparity.empty()) {
+        cerr << "Cannot read the disparity map files." << endl;
+
+        return 1;
+    }
+
+    // canvas = Mat(image.rows * 1.5, image.cols * 1.5, CV_8UC3);
+    canvas = Mat(image.size(), image.type());
+
+    namedWindow("cloud", WINDOW_AUTOSIZE);
+
+    // create trackbars for the camera position
+    createTrackbar("alpha",    "cloud", &tracker_alpha, 360, render);
+    createTrackbar("beta",     "cloud", &tracker_beta,  360, render);
+    createTrackbar("zoom",     "cloud", &tracker_zoom,  200, render);
+    createTrackbar("distance", "cloud", &tracker_dist,  200, render);
+
+    // initial rendering of the scene
+    render(0, NULL);
+
+    while (true) {
+        // wait for a key stroke; the same function arranges events processing
+        if ((uchar) waitKey(0) == 27) {
+            break;
+        }
+    }
+
+    return 0;
+}
